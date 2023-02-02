d7957499905c35d8a53be02734e66e87ea83661d
Etienne Orliac <etienne.orliac@epfl.ch>
2023-02-02 14:54:56 +0100

    Updated activate.sh and deactivate.sh for izar

--- env-bipp-izar/deactivate.sh
+++ env-bipp-izar/deactivate.sh
@@ -17,7 +17,7 @@ unset CUDA_HOME;
 export LD_LIBRARY_PATH=/work/scitas-ge/orliac/bin/lib:.;
 unset M4;
 export MANPATH=.:.:;
-export PATH=/work/ska/orliac/spack/bin:/work/scitas-ge/orliac/bin/bin:/home/orliac/.local/bin:/home/orliac/bin:/opt/clmgr/sbin:/opt/clmgr/bin:/opt/sgi/sbin:/opt/sgi/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/c3/bin:/opt/ibutils/bin:/sbin:/bin;
+export PATH=/work/ska/orliac/spack/bin:/opt/clmgr/sbin:/opt/clmgr/bin:/opt/sgi/sbin:/opt/sgi/bin:/work/scitas-ge/orliac/bin/bin:/home/orliac/.local/bin:/home/orliac/bin:/usr/lib64/ccache:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/c3/bin:/opt/ibutils/bin:/sbin:/bin;
 export PKG_CONFIG_PATH='';
 unset PROJ_LIB;
 export PYTHONPATH='';
